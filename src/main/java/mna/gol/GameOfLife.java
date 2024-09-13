2e1f959ddee6975315456fd0dd9cbf4ebda2b9c1
campai <the.campai@gmail.com>
2024-09-13 10:45:52 +0200

    doc: add game rules info

--- src/main/java/mna/gol/GameOfLife.java
+++ src/main/java/mna/gol/GameOfLife.java
@@ -13,6 +13,19 @@ import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.*;
 
+/**
+ * The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, alive or dead, or "populated" or "unpopulated". Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:
+ * <p>
+ * 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+ * <p>
+ * 2. Any live cell with two or three live neighbours lives on to the next generation.
+ * <p>
+ * 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+ * <p>
+ * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+ * <p>
+ * Source: <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Wikipedia: Game Of Life</a>
+ */
 @Slf4j
 public class GameOfLife extends JPanel implements KeyListener {
     @Serial
